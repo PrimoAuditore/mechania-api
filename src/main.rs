1cb582c480b08380620ac6c418e0a99571e25b31
Ezio Tobioli <ezio.tobioli@bairesdev.com>
2023-05-21 02:51:14 -0400

    fix: removed harcoded host of reveniu api

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod vehicle_handler;
 use handlers::{calculate_plan_handler, create_plan, get_plan, test_create};
 use plan_handlers::{create_plan_handler, get_plan_by_id_handler};
 use quote_handlers::{create_quote, get_quote};
+use sentry::integrations::panic::PanicIntegration;
 use tower_http::cors::CorsLayer;
 use vehicle_handler::get_vehicle_data;
 
@@ -20,10 +21,14 @@ use axum::{
 
 #[tokio::main]
 async fn main() {
-    let _guard = sentry::init(("https://7f2f98fe51504514a5ad0eceba3c9d03@o1166558.ingest.sentry.io/4505213614161920", sentry::ClientOptions {
-        release: sentry::release_name!(),
-        ..Default::default()
-    }));
+    let _guard = sentry::init((
+        "https://7f2f98fe51504514a5ad0eceba3c9d03@o1166558.ingest.sentry.io/4505213614161920",
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            ..Default::default()
+        }
+        .add_integration(PanicIntegration::new()),
+    ));
 
     // build our application with a single route
     let app = Router::new()
