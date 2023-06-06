6ba3c53aaef5be1ea4bbb9dc11430ed7c8129ceb
PrimoAuditore <122577819+PrimoAuditore@users.noreply.github.com>
2023-06-05 22:03:22 -0400

    Dynamic sentry environment (#13)
    * Iva included on final price, made error clearer
    
    * feat: Added new env var to set sentry environment
    
    * fix: made sentry environment to correspond to pull request id on dev environments

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ mod quote_handlers;
 mod sql;
 mod structs;
 mod vehicle_handler;
-use handlers::{calculate_plan_handler, create_plan, get_plan, test_create};
 use plan_handlers::{create_plan_handler, get_plan_by_id_handler};
 use quote_handlers::{create_quote, get_quote};
 use sentry::integrations::panic::PanicIntegration;
@@ -24,6 +23,7 @@ async fn main() {
     let _guard = sentry::init((
         "https://7f2f98fe51504514a5ad0eceba3c9d03@o1166558.ingest.sentry.io/4505213614161920",
         sentry::ClientOptions {
+            environment: Some(std::env::var("SENTRY_ENVIRONMENT").unwrap().into()),
             release: sentry::release_name!(),
             ..Default::default()
         }
@@ -33,13 +33,6 @@ async fn main() {
     // build our application with a single route
     let app = Router::new()
         .route("/health", get(|| async { "Hello, World!" }))
-        .layer(
-            CorsLayer::new()
-                .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
-                .allow_headers([http::header::CONTENT_TYPE])
-                .allow_methods([Method::GET, Method::POST]),
-        )
-        .route("/test", get(test_create))
         .layer(
             CorsLayer::new()
                 .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
